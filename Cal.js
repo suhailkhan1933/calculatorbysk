390ad41bc6c312ab24ad247c4d35098bcc385de4
suhailkhan1933 <themrone33@gmail.com>
2025-11-28 17:11:33 +0530

    calculator-pj-by-sk

---
+++ Cal.js
@@ -0,0 +1,25 @@
+let input = document.querySelector('inp');
+let button = document.querySelectorAll('button');
+
+
+let string = "";
+
+let arr = Array.from(button);
+arr.forEach( button =>{
+    button.addEventListener('click',(e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            inp.value =string;
+        }
+
+        else if (e.target.innerHTML == 'AC'){
+            string ="";
+            inp.value = string;
+        }
+        else{
+        string += e.target.innerHTML;
+        inp.value = string;
+        }
+    })
+})
+
